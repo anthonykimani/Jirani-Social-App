9602a484499d91ad681fa92c6e39d46e170b28f4
anthonykimani <anthonykimani@student.moringaschool.com>
2022-07-25 18:39:32 +0300

    Setups up react routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,14 @@
+import {BrowserRouter as Router,Route,Routes} from "react-router-dom";
+import Home from "../pages/Home";
 
-function App() {
+const App = ()=>{
   return (
     <div className="App">
-      
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,9 @@
 const Home = () => {
-    return (  );
+    return ( 
+        <div>
+            Hello World
+        </div>
+     );
 }
  
 export default Home;
