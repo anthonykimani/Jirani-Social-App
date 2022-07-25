9a3e3b9abd29b97523d461d0c551dd821eedd9db
anthonykimani <anthonykimani@student.moringaschool.com>
2022-07-25 21:48:32 +0300

    Styles the Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,18 @@
+import profilePic from '../images/profile.jpg'
+
 const Navbar = () => {
     return ( 
-        <div>
+        <div className="navbar-container">
             <div className="logo-container">
-                <h2>Jirani Social</h2>
+                <h2>Jirani Connect</h2>
             </div>
             <div className="search-bar-container">
-                <i class='bx bx-search'></i>
-                <input type="text" />
+                <i class='bx bx-search bx-fw'></i>
+                <input type="text" placeholder="Search for friends , posts" />
             </div>
             <div className="profile-container">
                 <button className="create-button">Create</button>
-                <img src="" alt="" />
+                <img src={profilePic} alt="" />
             </div>
         </div>
      );
