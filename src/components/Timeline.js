810f22ef8ef3547498a9f620dd91f9f15c7b3ab2
anthonykimani <anthonykimani@student.moringaschool.com>
2022-07-25 23:35:27 +0300

    Creates  Posts , CreatePost  components

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,7 +1,13 @@
+import Stories from "./Stories";
+import CreatePost from "./CreatePost";
+import Posts from "./Posts";
+
 const Timeline = () => {
     return ( 
         <div>
-            
+            <Stories />
+            <CreatePost />
+            <Posts />
         </div>
     );
 }
