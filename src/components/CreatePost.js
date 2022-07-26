05879ce4892e3d22597d7b50e4134935d4cb7534
anthonykimani <anthonykimani@student.moringaschool.com>
2022-07-26 09:01:56 +0300

    Add jsx to Posts components

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,7 +1,11 @@
+import profilePic from '../images/profile.jpg'
+
 const CreatePost = () => {
     return ( 
-        <div>
-            
+        <div className="createpost-container">
+            <img src={profilePic} alt="" />
+            <input type="text" placeholder='Whats on your mind? ' />
+            <button className="create-button">Post</button>
         </div>
      );
 }
--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,7 +1,17 @@
+import blackModel from "../images/close-up-model-wearing-make-up.jpg"
+import effielTower from "../images/close-up-picnic-near-eiffel-tower.jpg"
+import summer from "../images/summer-time-beach-night.jpg"
+import blackLady from  "../images/young-woman-wearing-orange-dress-with-turban-ethnic-jewelry.jpg"
+
 const Stories = () => {
     return ( 
-        <div>
-            
+        <div className="stories-container">
+            <img src={blackModel} alt="" />
+            <img src={effielTower} alt="" />
+            <img src={summer} alt="" />
+            <img src={blackLady} alt="" />
+            <img src={blackModel} alt="" />
+            <img src={summer} alt="" />
         </div>
      );
 }
