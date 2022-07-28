69871d8fdb1d8755aaede4a53dd789ac6af8ab50
anthonykimani <anthonykimani@student.moringaschool.com>
2022-07-28 10:57:55 +0300

    Allows users to view messages in expanded format

--- src/components/Text.js
+++ src/components/Text.js
@@ -1,15 +1,36 @@
 // import profilePic from "public/images/profile.jpg";
+import React, { useState } from "react";
 
-const Text = ({message}) => {
+const Text = ({ message }) => {
+  const [openMessage, setOpenMessage] = useState(false);
+
+  const handleDisplay = () => {
+    setOpenMessage(!openMessage);
+  };
   return (
-    <div className="text">
+    <div className="text" onClick={handleDisplay}>
       <img src={process.env.PUBLIC_URL + "/profile.jpg"} alt="" />
-      <div className="text-text">
+      <div
+        className="text-text"
+        style={
+          openMessage
+            ? {
+                height: "60px",
+                backgroundColor: "var(--bg-grey-color)",
+                padding: "20px",
+              }
+            : { display: "block" }
+        }
+      >
         <h4>{message.name}</h4>
         <p>{message.message}</p>
       </div>
       <div className="message-expand">
-        <i className="bx bx-chevron-down"></i>
+        <i
+          className={
+            openMessage ? "bx bx-chevron-up bx-sm" : "bx bx-chevron-down bx-sm"
+          }
+        ></i>
       </div>
     </div>
   );
